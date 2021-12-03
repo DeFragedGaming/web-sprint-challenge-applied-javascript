88799dfe2b75450614086d1723f4bf566c84a3e1
Matthew Hammel <mattyhammel17@gmail.com>
2021-12-03 13:02:03 -0700

    finished header.js 2 failed 9 passed

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,27 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
+  const header = document.createElement('div');
+  const dateSpan = document.createElement('span');
+  const headerTitle = document.createElement('h1');
+  const tempSpan = document.createElement('span');
+  
+  // necessary classes
+  header.classList.add('header');
+  dateSpan.classList.add('date');
+  tempSpan.classList.add('temp');
+
+  // text content
+  dateSpan.textContent = `${date}`;
+  headerTitle.textContent = `${title}`;
+  tempSpan.textContent = `${temp}`;
+
+  // final component structure 
+  header.appendChild(dateSpan);
+  header.appendChild(headerTitle);
+  header.appendChild(tempSpan);
+  console.log(header);
+  return header;
 }
 
 const headerAppender = (selector) => {
@@ -20,6 +41,8 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
+  const newHeader = Header('The First Header!', 'It\'s Friday', 'temp');
+  document.querySelector(selector).appendChild(newHeader);
 }
 
 export { Header, headerAppender }
